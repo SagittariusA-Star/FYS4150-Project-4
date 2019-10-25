f98281952037fe483791c4a0b95cea710dee5a56
Nils-Ole Stutzer <nilsole2009@live.no>
2019-10-25 10:56:37 +0200

    Adding MPI example

--- src/parallel_hello_world.cpp
+++ src/parallel_hello_world.cpp
@@ -1,1 +1,21 @@
-# 
+# include <mpi.h> 
+# include <iostream>
+
+
+
+
+int main(int argc, char *argv[])
+{
+    int numProcs, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int sum = 2;
+
+    MPI_Reduce(&rank, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    std::cout << rank << " " << sum << std::endl;
+    MPI_Finalize();
+
+    return 0;
+}

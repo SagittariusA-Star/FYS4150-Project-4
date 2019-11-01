63bf29380b1a44fd0a15b4fb1bce68fa3ecbaff9
bernharl <bernharl@student.matnat.uio.no>
2019-11-01 16:05:05 +0100

    Split mainprog into different scripts. Need to add this to makefile and make a main script

---
+++ src/task_b.cpp
@@ -0,0 +1,110 @@
+# include <fstream>
+# include <iostream>
+# include "ising.cpp"
+# include <stdexcept>
+# include <iomanip>
+# include <armadillo>
+# include <mpi.h>
+using std::cout;
+using std::ofstream;
+using std::setprecision;
+using std::setw;
+
+int main(int argc, char *argv[])
+{
+  int N;
+  int MC;
+  double T;
+  double E_mean;
+  double E_var;
+
+  MC = 1e6;
+  N = 2;
+  int T_len = 5;
+  double T_min = 1.0;
+  double T_max = 2.4;
+  double dT = (T_max - T_min)/((double) T_len);
+
+  int numbProc;
+  int rank;
+
+  double *T_array = new double[T_len];
+
+  for (int i = 0; i < T_len; i++)
+  {
+      T_array[i] = T_min + dT * i;
+  }
+
+  double *C_result = new double[T_len];
+  double *Chi_result = new double[T_len];
+  double *E_result = new double[T_len];
+  double *M_result = new double[T_len];
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numbProc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  arma::imat matrix = lattice(N);
+  double *results = new double[5];
+  double *C_array = new double[T_len];
+  double *Chi_array = new double[T_len];
+  double *E_mean_array = new double[T_len];
+  double *M_mean_array = new double[T_len];
+
+  for (int i = 0; i < T_len; i++)
+  {
+      C_array[i] = 0.0;
+      Chi_array[i] = 0.0;
+      E_mean_array[i] = 0.0;
+      M_mean_array[i] = 0.0;
+  }
+
+  double *E = new double[1];
+  double *M = new double[1];
+  double *accp_flips = new double[1];
+  double local_min = (int) std::round(T_len * rank / (double) numbProc);
+  double local_max = (int) std::round(T_len * (rank + 1) / (double) numbProc);
+
+  for (int i = local_min; i < local_max; i++)
+  {
+      metropolis(MC, N, 0, matrix, T_array[i], E, M, accp_flips, results, rank);
+      E_mean_array[i] = results[0];
+      M_mean_array[i] = results[4];
+      C_array[i] = results[1] / (T_array[i] * T_array[i]);
+      Chi_array[i] = results[3] / T_array[i];
+
+  }
+  MPI_Allreduce(C_array, C_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(Chi_array, Chi_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(E_mean_array, E_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(M_mean_array, M_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+  delete[] E;
+  delete[] M;
+  delete[] accp_flips;
+  delete[] results;
+  delete[] C_array;
+  delete[] Chi_array;
+  delete[] E_mean_array;
+  delete[] M_mean_array;
+
+  if (rank == 0)
+  {
+      ofstream outfile;
+      outfile.open("Ex_4b.txt");
+      outfile << " T: " << " <E>: " << "<|M|>: " << "C_V: " << "Chi: " << endl;
+      for (int i = 0; i < T_len; i++)
+      {
+          outfile << setprecision(10) << setw(20) << T_array[i]
+                  << setprecision(10) << setw(20) << E_result[i]
+                  << setprecision(10) << setw(20) << M_result[i]
+                  << setprecision(10) << setw(20) << C_result[i]
+                  << setprecision(10) << setw(20) << Chi_result[i] << endl;
+      }
+      outfile.close();
+      delete[] C_result;
+      delete[] Chi_result;
+      delete[] E_result;
+      delete[] M_result;
+  }
+   MPI_Finalize();
+}
---
+++ src/task_c_and_d.cpp
@@ -0,0 +1,95 @@
+# include <fstream>
+# include <iostream>
+# include "ising.cpp"
+# include <stdexcept>
+# include <iomanip>
+# include <armadillo>
+# include <mpi.h>
+using std::cout;
+using std::ofstream;
+using std::setprecision;
+using std::setw;
+
+int main(int argc, char *argv[])
+{
+    int N = 20;
+    int MC = 10; //1e6;
+    double T = 1.0;
+    double E_mean;
+    double E_var;
+    int T_len;
+    double T_min;
+    double T_max;
+    double dT;
+    int rank = 0;
+
+    arma::imat matrix = lattice(N);
+    double *E = new double[MC];
+    double *M = new double[MC];
+    double *accp_flips = new double[MC];
+    double *results = new double[5];
+    metropolis(MC, N, 0, matrix, T, E, M, accp_flips, results, rank);
+    ofstream outfile3;
+    outfile3.open("P_T1_MC1e7_Disordered.txt");
+    outfile3 << "E: " << "|M|" << "Accepted flips" <<  "E_var (Next line): " << endl;
+    outfile3 << results[1] << endl;
+    for (int i = 0; i < MC; i++)
+    {
+        outfile3 << setprecision(10) << setw(20) << E[i]
+                 << setprecision(10) << setw(20) << std::fabs(M[i])
+                 << setprecision(10) << setw(20) << accp_flips[i]
+                 << endl;
+    }
+    outfile3.close();
+
+    matrix.fill(1);
+    metropolis(MC, N, 0, matrix, T, E, M, accp_flips, results, rank);
+    ofstream outfile4;
+    outfile4.open("P_T1_MC1e7_Ordered.txt");
+    outfile4 << "E: " << "|M|" << "Accepted flips" <<  "E_var (Next line): " << endl;
+    outfile4 << results[1] << endl;
+    for (int i = 0; i < MC; i++)
+    {
+        outfile4 << setprecision(10) << setw(20) << E[i]
+                 << setprecision(10) << setw(20) << std::fabs(M[i])
+                 << setprecision(10) << setw(20) << accp_flips[i]
+                 << endl;
+    }
+    outfile4.close();
+
+
+    T = 2.4;
+    matrix = lattice(N);
+    metropolis(MC, N, 0, matrix, T, E, M, accp_flips, results, rank);
+    ofstream outfile5;
+    outfile5.open("P_T24_MC1e7_Disordered.txt");
+    outfile5 << "E: " << "|M|" << "Accepted flips" <<  "E_var (Next line): " << endl;
+    outfile5 << results[1] << endl;
+    for (int i = 0; i < MC; i++)
+    {
+        outfile5 << setprecision(10) << setw(20) << E[i]
+                 << setprecision(10) << setw(20) << std::fabs(M[i])
+                 << setprecision(10) << setw(20) << accp_flips[i]
+                 << endl;
+    }
+    outfile5.close();
+
+    matrix.fill(1);
+    metropolis(MC, N, 0, matrix, T, E, M, accp_flips, results, rank);
+    ofstream outfile6;
+    outfile6.open("P_T24_MC1e7_Ordered.txt");
+    outfile6 << "E: " << "|M|" << "Accepted flips" <<  "E_var (Next line): " << endl;
+    outfile6 << results[1] << endl;
+    for (int i = 0; i < MC; i++)
+    {
+        outfile6 << setprecision(10) << setw(20) << E[i]
+                 << setprecision(10) << setw(20) << std::fabs(M[i])
+                 << setprecision(10) << setw(20) << accp_flips[i]
+                 << endl;
+    }
+    outfile6.close();
+    delete[] E;
+    delete[] M;
+    delete[] accp_flips;
+
+}

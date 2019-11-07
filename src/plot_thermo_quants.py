e839ce42bcc3abc20f270000110167201de7ed1e
Nils-Ole Stutzer <nilsole2009@live.no>
2019-11-07 14:15:38 +0100

    Started on report

--- src/plot_thermo_quants.py
+++ src/plot_thermo_quants.py
@@ -31,6 +31,7 @@ M60 = L60[:, 2] / N60
 C_v60 = L60[:, 3] / N60
 Chi60 = L60[:, 4] / N60
 
+T2 = L80[:, 0]
 N80 = 80 ** 2
 E80 = L80[:, 1] / N80
 M80 = L80[:, 2] / N80
@@ -47,8 +48,8 @@ Chi100 = L100[:, 4] / N100
 fig, ax = plt.subplots(2, 2, sharex = True)
 ax[0, 0].scatter(T, E40, label = "N = 40", color = "r", s = 10)
 ax[0, 0].scatter(T, E60, label = "N = 60", color = "b", s = 10)
-ax[0, 0].scatter(T, E80, label = "N = 80", color = "g", s = 10)
-ax[0, 0].scatter(T, E100, label = "N = 100", color = "orange", s = 10)
+ax[0, 0].scatter(T2, E80, label = "N = 80", color = "g", s = 10)
+ax[0, 0].scatter(T2, E100, label = "N = 100", color = "orange", s = 10)
 ax[0, 0].legend(loc = 0)
 ax[0, 0].set_ylabel(r"$\frac{\langle E(T) \rangle}{N^2}$")
 #ax[0, 0].set_xlabel(r"$T$")
@@ -56,8 +57,8 @@ ax[0, 0].grid()
 
 ax[1, 0].scatter(T, M40, label = "N = 40", color = "r", s = 10)
 ax[1, 0].scatter(T, M60, label = "N = 60", color = "b", s = 10)
-ax[1, 0].scatter(T, M80, label = "N = 80", color = "g", s = 10)
-ax[1, 0].scatter(T, M100, label = "N = 100", color = "orange", s = 10)
+ax[1, 0].scatter(T2, M80, label = "N = 80", color = "g", s = 10)
+ax[1, 0].scatter(T2, M100, label = "N = 100", color = "orange", s = 10)
 ax[1, 0].legend(loc = 0)
 ax[1, 0].set_ylabel(r"$\frac{\langle |M(T)| \rangle}{N^2}$")
 ax[1, 0].set_xlabel(r"$T$")
@@ -65,8 +66,8 @@ ax[1, 0].grid()
 
 ax[0, 1].scatter(T, C_v40, label = "N = 40", color = "r", s = 10)
 ax[0, 1].scatter(T, C_v60, label = "N = 60", color = "b", s = 10)
-ax[0, 1].scatter(T, C_v80, label = "N = 80", color = "g", s = 10)
-ax[0, 1].scatter(T, C_v100, label = "N = 100", color = "orange", s = 10)
+ax[0, 1].scatter(T2, C_v80, label = "N = 80", color = "g", s = 10)
+ax[0, 1].scatter(T2, C_v100, label = "N = 100", color = "orange", s = 10)
 ax[0, 1].legend(loc = 0)
 ax[0, 1].set_ylabel(r"$\frac{C_v(T)}{N^2}$")
 #ax[0, 1].set_xlabel(r"$T$")
@@ -74,8 +75,8 @@ ax[0, 1].grid()
 
 ax[1, 1].scatter(T, Chi40, label = "N = 40", color = "r", s = 10)
 ax[1, 1].scatter(T, Chi60, label = "N = 60", color = "b", s = 10)
-ax[1, 1].scatter(T, Chi80, label = "N = 80", color = "g", s = 10)
-ax[1, 1].scatter(T, Chi100, label = "N = 100", color = "orange", s = 10)
+ax[1, 1].scatter(T2, Chi80, label = "N = 80", color = "g", s = 10)
+ax[1, 1].scatter(T2, Chi100, label = "N = 100", color = "orange", s = 10)
 ax[1, 1].legend(loc = 0)
 ax[1, 1].set_ylabel(r"$\frac{\chi(T)}{N^2}$")
 ax[1, 1].set_xlabel(r"$T$")
@@ -94,5 +95,5 @@ TC80 = T[np.argmax(C_v80)]
 a = (TC100 - TC80) / (L100 ** (-1 / nu) - L80 ** (-1 / nu)) 
 TC = TC100 - a * L100 ** (-1 / nu)
 TC_analytical = 2.269
-epsilon = abs(TC - TC_analytical) / TC_analytical
-print("Critical Temperature: ", TC, " Relative error: ", epsilon)
+epsilon = abs(TC - TC_analytical) / TC_analytical * 100
+print("Critical Temperature: ", TC, " Relative error: ", epsilon, " %")
--- src/task_e.cpp
+++ src/task_e.cpp
@@ -12,30 +12,30 @@ using std::setw;
 
 int main(int argc, char *argv[])
 {
-    int N = 60;
+    int N = 40;
     int MC = 1e6;
     double T;
     double E_mean;
     double E_var;
-    int T_len = 50;
+    int T_len = 81;
     double T_min = 2.0;
     double T_max = 2.5;
     double dT = (T_max - T_min)/((double) T_len);;
     int numbProc;
     int rank;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numbProc);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    if (rank == 0)
-    {
-        cout << "Starting computation for L = 60" << endl;
-    }
     double *T_array = new double[T_len];
-
     for (int i = 0; i < T_len; i++)
     {
         T_array[i] = T_min + dT * i;
+    }
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numbProc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    if (rank == 0)
+    {
+        cout << "Starting computation for L = 40" << endl;
     }
 
     double *C_result = new double[T_len];
@@ -61,9 +61,59 @@ int main(int argc, char *argv[])
 
     double *E = new double[1];
     double *M = new double[1];
-    double *accp_flips = new double[1];
+    double *accp_flips = new double[1]; 
     int local_min = (int) std::round(T_len * rank / (double) numbProc);
     int local_max = (int) std::round(T_len * (rank + 1) / (double) numbProc);
+
+    /*
+    for (int i = local_min; i < local_max; i++)    
+    {   
+        metropolis(MC, N, 5e3, matrix, T_array[i], E, M, accp_flips, results, rank);
+        E_mean_array[i] = results[0];
+        M_mean_array[i] = results[4];
+        C_array[i] = results[1] / (T_array[i] * T_array[i]);
+        Chi_array[i] = results[3] / T_array[i];
+
+    }
+    MPI_Allreduce(C_array, C_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(Chi_array, Chi_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(E_mean_array, E_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(M_mean_array, M_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {   
+        cout << "Printing to file: " << endl;
+        ofstream outfile;
+        outfile.open("L40.txt");
+        outfile << " T: " << " <E>: " << "<|M|>: " << "C_V: " << "Chi: " << endl;
+        for (int i = 0; i < T_len; i++)
+        {   
+            outfile << setprecision(10) << setw(20) << T_array[i]
+                    << setprecision(10) << setw(20) << E_result[i]
+                    << setprecision(10) << setw(20) << M_result[i]
+                    << setprecision(10) << setw(20) << C_result[i]
+                    << setprecision(10) << setw(20) << Chi_result[i] << endl;
+        }
+        outfile.close();
+    }
+
+    
+    if (rank == 0)
+    {
+        cout << "Starting computation for L = 60" << endl;
+    }
+    
+    N = 60;
+
+    matrix = lattice(N);
+
+    for (int i = 0; i < T_len; i++)
+    {
+        C_array[i] = 0.0;
+        Chi_array[i] = 0.0;
+        E_mean_array[i] = 0.0;
+        M_mean_array[i] = 0.0;
+    }
 
     for (int i = local_min; i < local_max; i++)
     {
@@ -103,42 +153,16 @@ int main(int argc, char *argv[])
                     << setprecision(10) << setw(20) << Chi_result[i] << endl;
         }
         outfile.close();
-        delete[] C_result;
-        delete[] Chi_result;
-        delete[] E_result;
-        delete[] M_result;
     }
-
+    
     if (rank == 0)
     {
         cout << "Starting computation for L = 80" << endl;
     }
 
-
     N = 80;
-    T_len = 50;
-    T_min = 2.0;
-    T_max = 2.5;
-    dT = (T_max - T_min)/((double) T_len);
-    T_array = new double[T_len];
-
-    for (int i = 0; i < T_len; i++)
-    {
-        T_array[i] = T_min + dT * i;
-    }
-
-    C_result = new double[T_len];
-    Chi_result = new double[T_len];
-    E_result = new double[T_len];
-    M_result = new double[T_len];
 
     matrix = lattice(N);
-
-    results = new double[5];
-    C_array = new double[T_len];
-    Chi_array = new double[T_len];
-    E_mean_array = new double[T_len];
-    M_mean_array = new double[T_len];
 
     for (int i = 0; i < T_len; i++)
     {
@@ -147,12 +171,6 @@ int main(int argc, char *argv[])
         E_mean_array[i] = 0.0;
         M_mean_array[i] = 0.0;
     }
-
-    E = new double[1];
-    M = new double[1];
-    accp_flips = new double[1];
-    local_min = (int) std::round(T_len * rank / (double) numbProc);
-    local_max = (int) std::round(T_len * (rank + 1) / (double) numbProc);
 
     for (int i = local_min; i < local_max; i++)
     {
@@ -167,15 +185,6 @@ int main(int argc, char *argv[])
     MPI_Allreduce(Chi_array, Chi_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(E_mean_array, E_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(M_mean_array, M_result, T_len, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-    delete[] E;
-    delete[] M;
-    delete[] accp_flips;
-    delete[] results;
-    delete[] C_array;
-    delete[] Chi_array;
-    delete[] E_mean_array;
-    delete[] M_mean_array;
 
     if (rank == 0)
     {
@@ -192,42 +201,16 @@ int main(int argc, char *argv[])
                     << setprecision(10) << setw(20) << Chi_result[i] << endl;
         }
         outfile.close();
-        delete[] C_result;
-        delete[] Chi_result;
-        delete[] E_result;
-        delete[] M_result;
     }
-
+    */
     if (rank == 0)
     {
         cout << "Starting computation for L = 100" << endl;
     }
 
-
     N = 100;
-    T_len = 50;
-    T_min = 2.0;
-    T_max = 2.5;
-    dT = (T_max - T_min)/((double) T_len);
-    T_array = new double[T_len];
-
-    for (int i = 0; i < T_len; i++)
-    {
-        T_array[i] = T_min + dT * i;
-    }
-
-    C_result = new double[T_len];
-    Chi_result = new double[T_len];
-    E_result = new double[T_len];
-    M_result = new double[T_len];
-
+    
     matrix = lattice(N);
-
-    results = new double[5];
-    C_array = new double[T_len];
-    Chi_array = new double[T_len];
-    E_mean_array = new double[T_len];
-    M_mean_array = new double[T_len];
 
     for (int i = 0; i < T_len; i++)
     {
@@ -236,12 +219,6 @@ int main(int argc, char *argv[])
         E_mean_array[i] = 0.0;
         M_mean_array[i] = 0.0;
     }
-
-    E = new double[1];
-    M = new double[1];
-    accp_flips = new double[1];
-    local_min = (int) std::round(T_len * rank / (double) numbProc);
-    local_max = (int) std::round(T_len * (rank + 1) / (double) numbProc);
 
     for (int i = local_min; i < local_max; i++)
     {
@@ -281,12 +258,13 @@ int main(int argc, char *argv[])
                     << setprecision(10) << setw(20) << Chi_result[i] << endl;
         }
         outfile.close();
-        delete[] C_result;
-        delete[] Chi_result;
-        delete[] E_result;
-        delete[] M_result;
+
     }
-
+    
+    delete[] C_result;
+    delete[] Chi_result;
+    delete[] E_result;
+    delete[] M_result;
+    delete[] T_array;
     MPI_Finalize();
-
 }
